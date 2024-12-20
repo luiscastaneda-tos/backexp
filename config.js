2dcc9e6086cc08db9ef30cc9b7cc72a51d39556b
Angel Castañeda <luis.castaneda@noktos.com>
2024-12-20 11:32:05 -0600

    DONE CONSOLE

--- config.js
+++ config.js
@@ -8,18 +8,4 @@ const config = {
     },
     listPerPage: 12
 }
-/*
-CREATE TABLE Concurso (
-    id VARCHAR(40) PRIMARY KEY,                -- id como clave primaria
-    nombres VARCHAR(100),               -- Nombres del cliente
-    apellidos VARCHAR(100),             -- Apellidos del cliente
-    folio INT UNIQUE,                   -- Folio único
-    correo VARCHAR(255),                -- Correo electrónico
-    telefono VARCHAR(15),               -- Teléfono
-    codigo_postal VARCHAR(10),          -- Código postal
-    ciudad VARCHAR(100),                -- Ciudad
-    sucursal ENUM('Aragon', 'Pena Pena', 'Izazaga') -- Sucursal
-);
-*/
-
 module.exports = config;
